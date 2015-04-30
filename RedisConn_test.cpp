82a46bef7a0a1be47b68e13767b0710972a0ab44
nanrenhaiyang <yuhaiyang457288@sina.com>
2015-04-30 13:56:36 +0800

    增加单元测试文件　RedisConn_test.cpp

---
+++ RedisConn_test.cpp
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2015, 爱wifi（版权声明）
+ *
+ * @file	RedisConn_test.cpp		
+ * @brief 此文件用于对　RedisConn 类进行测试。
+ *
+ * 此文件用于对　RedisConn 类进行测试。
+ *
+ * @author: 		yuhaiyang
+ * @date: 		2015年4月30日
+ *
+ * 修订说明:初始版本
+ */
+
+int main()
+{
+
+}
+
+
