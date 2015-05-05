134439734fbe8844f59f38c7b773c4dc481a3a83
cj8640 <3122282459@qq.com>
2015-05-04 19:27:19 -0700

    add comment by cj

--- CRedisConn.h
+++ CRedisConn.h
@@ -31,25 +31,20 @@
 	typeName & operator=( const typeName & )
 #endif
 
-namespace RedisCpp
-{
+namespace RedisCpp {
 
 typedef std::list<std::string> ValueList;
-typedef std::map<std::string,std::string> ValueMap;
+typedef std::map<std::string, std::string> ValueMap;
 
-class NullReplyException : public std::runtime_error
-{
+class NullReplyException: public std::runtime_error {
 public:
-	NullReplyException( ):
-		std::runtime_error( "reply NULL" )
-	{
+	NullReplyException() :
+			std::runtime_error("reply NULL") {
 	}
 };
 
-
 ///< 这个是插入时候是插入在指定元素之前，或者指定元素之后
-typedef enum INSERT_POS
-{
+typedef enum INSERT_POS {
 	BEFORE,			///< 插入到指定元素之前。
 	AFTER
 } E_INSERT_POS;
@@ -59,29 +54,28 @@ typedef enum INSERT_POS
  * 
  * 例子代码:
  *int main( )
-{
-	RedisCpp::RedisConn con;
-	if ( !con.connect( "127.0.0.1", 6379, "521110", 5 ) )
-	{
-		std::cout << "connect error " << con.getErrorStr( ) << std::endl;
-		return 0;
-	}
+ {
+ RedisCpp::RedisConn con;
+ if ( !con.connect( "127.0.0.1", 6379, "521110", 5 ) )
+ {
+ std::cout << "connect error " << con.getErrorStr( ) << std::endl;
+ return 0;
+ }
 
-	std::string value;
-	if ( !con.hget( "newHash", "yuhaiyang", value ) )
-	{
-		std::cout << "hget error " << con.getErrorStr( ) << std::endl;
-	}
-	return 0;
-}
+ std::string value;
+ if ( !con.hget( "newHash", "yuhaiyang", value ) )
+ {
+ std::cout << "hget error " << con.getErrorStr( ) << std::endl;
+ }
+ return 0;
+ }
  *
  */
-class CRedisConn
-{
+class CRedisConn {
 public:
-	CRedisConn( void );
+	CRedisConn(void);
 
-	virtual ~CRedisConn( void );
+	virtual ~CRedisConn(void);
 
 	/**
 	 *@brief 初始化链接信息
@@ -89,8 +83,8 @@ public:
 	 *用于初始化链接的详细信息。在　connect() 之前使用。也可以直接调用
 	 *bool connect( const std::string &host ,const uint16_t port, const std::string& password , const uint32_t timeout );来初始化。
 	 */
-	void init( const std::string &host="127.0.0.1" ,const uint16_t port=6379 ,
-						const std::string& pass="" , const uint32_t timeout=0 );
+	void init(const std::string &host = "127.0.0.1", const uint16_t port = 6379,
+			const std::string& pass = "", const uint32_t timeout = 0);
 
 	/**
 	 *@brief 链接　redis 数据库
@@ -99,12 +93,12 @@ public:
 	 *
 	 *@return 成功返回　true,失败返回　false
 	 */
-	bool connect(  );
+	bool connect();
 
 	/**
 	 *@brief 断开与　redis 内存数据库链接
 	 */
-	void disConnect( );
+	void disConnect();
 
 	/**
 	 *@brief 链接　redis 数据库之后，需要认证。
@@ -112,7 +106,7 @@ public:
 	 *@param [in] 登录的密码。
 	 *@return 成功返回　true, 失败返回　false.
 	 */
-	bool auth( const std::string& password );
+	bool auth(const std::string& password);
 
 	/**
 	 *@brief 初始化链接信息并且链接。
@@ -125,41 +119,38 @@ public:
 	 *@param [in] timeout . 链接的超时时间。
 	 *@return true 成功，　false 失败。
 	 */
-	bool connect( const std::string &host ,const uint16_t port,
-					const std::string& password="" , const uint32_t timeout=0 );
+	bool connect(const std::string &host, const uint16_t port,
+			const std::string& password = "", const uint32_t timeout = 0);
 
 	/**
 	 * @brief 检查与服务器网络
 	 *
 	 * @return 网络链接正常返回　true, 不通返回　false.
 	 */
-	bool ping( ) ;
+	bool ping();
 
 	/**
 	 *@brief 重新链接　redis 服务器。
 	 *
 	 *@return 重连成功　true, 重连失败　false.
 	 */
-	bool reconnect( ) ;
+	bool reconnect();
 
 	/**
 	 * @brief 检查是否链接过　redis 服务器。
 	 *
 	 * @return 已经链接　true,没有链接过　false.
 	 */
-	inline bool isConneced( )
-	{
+	inline bool isConneced() {
 		return _connected;
 	}
-
-
 
 	/**
 	 * @brief 获取错误的原因
 	 *
 	 * ＠return 返回发生错误的原因.
 	 */
-	const std::string getErrorStr( ) const;
+	const std::string getErrorStr() const;
 
 	/**
 	 * @brief 直接发送指令参数给　redis 服务器。
@@ -168,36 +159,73 @@ public:
 	 * ＠param [in] ... 指令及数据字符串。
 	 * @return NULL ,redis 执行失败，或者断开俩就。橙光返回　redisReply 指针。
 	 */
-	redisReply*  redisCmd( const char *format, ... );
+	redisReply* redisCmd(const char *format, ...);
 
 	///////////////////////////////// list 的方法 /////////////////////////////////////
-	bool lpush( const std::string& key , const std::string& value , uint64_t& retval )
-	                throw ( NullReplyException );
+	bool lpush(const std::string& key, const std::string& value,
+			uint64_t& retval) throw (NullReplyException);
 
-	bool lpop(const std::string& key, std::string& value );
+	bool lpop(const std::string& key, std::string& value);
 
-	bool lrange( const std::string &key, uint32_t start, int32_t end, ValueList& valueList );
+	bool lrange(const std::string &key, uint32_t start, int32_t end,
+			ValueList& valueList);
 
-	bool rpush( const std::string& key , const std::string& value , uint64_t& retval );
+	bool rpush(const std::string& key, const std::string& value,
+			uint64_t& retval);
 
-	bool rpop( const std::string& key , std::string& value );
+	bool rpop(const std::string& key, std::string& value);
 
-	bool linsert( const std::string& key , INSERT_POS position ,
-	                const std::string& pivot , const std::string value , int64_t& retval );
+	bool linsert(const std::string& key, INSERT_POS position,
+			const std::string& pivot, const std::string value, int64_t& retval);
 
-	bool lindex( const std::string& key , int32_t index , std::string& value );
+	bool lindex(const std::string& key, int32_t index, std::string& value);
 
-	bool llen( const std::string& key , uint64_t& retval );
+	bool llen(const std::string& key, uint64_t& retval);
 
 	//////////////////////////////   hash 的方法 //////////////////////////////////////
 
-	bool hget( const std::string& key , const std::string& filed , std::string& value );
+	/**
+	 * @brief从哈希表中取出以key和field所对应的value值
+	 * @param [in] key 是键名，相当于表名
+	 * @param [in] filed 是字段名
+	 * @param [out] value 是获取的值
+	 * @return true 成功获取，false获取失败
+	 * @warning 获取失败 value为""(string初始化默认值)
+	 */
+	bool hget(const std::string& key, const std::string& filed,
+			std::string& value);
 
-	bool hset( const std::string& key , const std::string& filed , const std::string& value ,uint32_t& retval );
+	/**
+	 * @brief设置哈希表中以key和field所对应的value值
+	 * @param [in] key 是键名，相当于表名
+	 * @param [in] filed 是字段名
+	 * @param [in] value 是以上两参数对应的值
+	 * @param [out] retval 获取的值
+	 * @return true 成功获取，false获取失败
+	 * @warning 设置失败 retval为0，成功为1
+	 */
+	bool hset(const std::string& key, const std::string& filed,
+			const std::string& value, uint32_t& retval);
 
-	bool hdel( const std::string& key , const std::string& filed , uint32_t& retval );
+	/**
+	 * @brief删除哈希表中key所对应的field这一项内容
+	 * @param [in] key 是键名，相当于表名
+	 * @param [in] filed 是字段名
+	 * @param [out] retval 获取的值
+	 * @return true 成功获取，false获取失败
+	 *@warning 删除失败 retval为0，成功为1
+	 */
+	bool hdel(const std::string& key, const std::string& filed,
+			uint32_t& retval);
 
-	bool hgetall( const std::string& key , ValueMap& valueMap);
+	/**
+	 * @brief取得哈希表中key所对应的所有内容
+	 * @param [in] key 是键名，相当于表名
+	 * @param [out] valueMap 获取的值（map<string,string>类型）
+	 * @return true 成功获取，false获取失败
+	 * @warning 获取失败 valueMap为空
+	 */
+	bool hgetall(const std::string& key, ValueMap& valueMap);
 
 protected:
 	/**
@@ -207,7 +235,7 @@ protected:
 	 *@param [out] valueList .从　reply 提取的多条数据.
 	 *@return 成功返回 true, 失败返回　false.
 	 */
-	bool _getArryToList( redisReply* reply , ValueList& valueList );
+	bool _getArryToList(redisReply* reply, ValueList& valueList);
 
 	/**
 	 *@brief  从 reply->type 为REDIS_REPLY_ARRY 类型的元素获取数据填充到valueMap 列表.
@@ -218,14 +246,13 @@ protected:
 	 *@param [out] valueMap .从 reply 提取的much of pair 数据存储到　valueMap.
 	 *@return 成功返回 true, 失败返回　false.
 	 */
-	bool _getArryToMap( redisReply* reply , ValueMap& valueMap );
+	bool _getArryToMap(redisReply* reply, ValueMap& valueMap);
 
-	bool _getError( const redisContext* redCtx );
+	bool _getError(const redisContext* redCtx);
 
-	bool _getError( const redisReply* reply );
+	bool _getError(const redisReply* reply);
 
-	inline redisContext* _getCtx( ) const
-	{
+	inline redisContext* _getCtx() const {
 		return _redCtx;
 	}
 
@@ -241,8 +268,7 @@ private:
 	bool _connected;			///< if connected
 
 	///< error number
-	enum E_ERROR_NO
-	{
+	enum E_ERROR_NO {
 		ERR_NO_ERROR = 0,
 		ERR_NULL,
 		ERR_NO_CONNECT,
@@ -254,13 +280,10 @@ private:
 	};
 	std::string _errStr;		///< Describe the reason for error..
 
-	static const char* _errDes[ ERR_BOTTOM ];	///< describe error
+	static const char* _errDes[ERR_BOTTOM];	///< describe error
 };
 
 } /* namespace RedisCpp */
 
 #endif /* REDISCONN_H_ */
-
-
-
 
